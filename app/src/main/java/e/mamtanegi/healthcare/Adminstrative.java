696c059e201658d38b77df160adc89bd4d0433f2
mamta <negimamta325@gmail.com>
2019-04-21 16:56:01 +0530

    Initial commit

---
+++ app/src/main/java/e/mamtanegi/healthcare/Adminstrative.java
@@ -0,0 +1,13 @@
+package e.mamtanegi.healthcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Adminstrative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminstrative);
+    }
+}
